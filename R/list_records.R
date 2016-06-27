66d275b3b7929cde6369cb8c55ba2f5e10264f3f
statwonk <cpeter9@gmail.com>
2016-06-27 15:41:36 +0000

    maxRecords and pagSize might be needed for large bases.

--- R/list_records.R
+++ R/list_records.R
@@ -1,60 +1,72 @@
-#' List records form Airtable 
+#' List records form Airtable
 #'
 #' \code{list_records} returns records by issuing GET request to a table endpoint
 #'
-#' @param air_options A list 
+#' @param air_options A list
 #' @param table A length-one character vector
 #' @param offset An optional length-one character vector
 #' @param recursive A length-one logical vector
 #' @param filter_by_formula An optional length-one character vector
 
-list_records <- 
-  function(
-    air_options, 
-    table, 
-    offset = NULL, 
+list_records <- function(
+    air_options,
+    table,
+    pageSize = NULL,
+    offset = NULL,
     recursive = TRUE,
-    filter_by_formula = NULL
-  ) { 
+    filter_by_formula = NULL,
+    maxRecords = NULL
+  ) {
 
   query <- list()
 
-  if (!is.null(offset)) {
-    query <- 
-      append(
-        query, 
+  if(!is.null(offset)) {
+    query <- append(
+        query,
         list(offset = offset)
       )
   }
 
-  if (!is.null(filter_by_formula)) {
-    query <- 
-      append(
-        query, 
+  if(!is.null(pageSize)) {
+    query <- append(
+      query,
+      list(pageSize = pageSize)
+    )
+  }
+
+  if(!is.null(maxRecords)) {
+    query <- append(
+      query,
+      list(maxRecords = maxRecords)
+    )
+  }
+
+  if(!is.null(filter_by_formula)) {
+    query <- append(
+        query,
         list(
           filterByFormula = filter_by_formula
         )
       )
   }
 
-  req <- 
-    dispatch_request(
-      air_options, 
-      table, 
-      method = "GET", 
+  req <- dispatch_request(
+      air_options,
+      table,
+      method = "GET",
       query = query
-    ) 
+    )
 
-  airtable_check(req) 
+  airtable_check(req)
 
   offset  <- httr::content(req)$offset
   records <- httr::content(req)$records
 
-  # recursively append next page records to `records` 
+  # recursively append next page records to `records`
   if (recursive && !is.null(offset)) {
     return(
       records <- append(
-        records, 
+        records,
         list_records(
           air_options,
           table,
@@ -63,8 +75,8 @@ list_records <-
         )
       )
     )
-  } 
+  }
 
   records
-} 
+}
 
